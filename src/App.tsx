8f9c1b88b0cd637aadf8eb912e3091d88e8fe826
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2022-05-11 11:14:54 +0200

    Add grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { SimulationControls, SimulationGrid } from './components'
 
 function App() {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center h-screen text-white bg-gray-800">
+    <div className="flex flex-col gap-8 justify-center items-center p-10 h-screen text-white bg-gray-800">
       <SimulationGrid />
       <SimulationControls />
     </div>
--- src/components/SimulationGrid.tsx
+++ src/components/SimulationGrid.tsx
@@ -1,5 +1,34 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState } from 'react'
 
 export const SimulationGrid: FunctionComponent = () => {
-  return <div>Grid</div>
+  const numCols = 20
+  const numRows = numCols
+
+  const generateEmptyGrid = () => {
+    const rows = []
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+    return rows
+  }
+
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid()
+  })
+
+  return (
+    <div
+      className="grid gap-1"
+      style={{ gridTemplateColumns: `repeat(${numCols}, minmax(0, 1fr))` }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+            className="w-8 h-8 bg-gray-600 active:bg-sky-800 rounded-md transition-all hover:scale-105"
+            key={`${i}-${k}`}
+          />
+        ))
+      )}
+    </div>
+  )
 }

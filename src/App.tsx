5b296520fbf44d3aad619aaeb3fbc2242e1c13ca
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2022-05-13 09:21:33 +0200

    Game initial setup (#2)
    * Add clear and random generation of cells
    
    * Add single cell selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import { SimulationControls, SimulationGrid } from './components'
+import { generateEmptyGrid, generateRandomGrid } from './utils'
 
 function App() {
+  const columnAmount = 15
+  const rowsAmount = 15
+
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid(rowsAmount, columnAmount)
+  })
+
   return (
     <div className="flex flex-col gap-8 justify-center items-center p-10 h-screen text-white bg-gray-800">
-      <SimulationGrid />
-      <SimulationControls />
+      <SimulationGrid grid={grid} setGrid={setGrid} />
+      <SimulationControls
+        onClear={() => {
+          setGrid(generateEmptyGrid(rowsAmount, columnAmount))
+        }}
+        onRandom={() => {
+          setGrid(generateRandomGrid(rowsAmount, columnAmount))
+        }}
+      />
     </div>
   )
 }
--- src/components/SimulationGrid.tsx
+++ src/components/SimulationGrid.tsx
@@ -1,25 +1,31 @@
-import { FunctionComponent, useState } from 'react'
+import classNames from 'classnames'
+import produce from 'immer'
+import { FunctionComponent } from 'react'
 
-export const SimulationGrid: FunctionComponent = () => {
-  const numCols = 20
-  const numRows = numCols
-
-  const generateEmptyGrid = () =>
-    [...Array(numRows)].map(() => Array(numCols).fill(0))
-
-  const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid()
-  })
-
+export const SimulationGrid: FunctionComponent<{
+  grid: number[][]
+  setGrid: (grid: number[][]) => void
+}> = ({ grid, setGrid }) => {
   return (
     <div
       className="grid gap-1"
-      style={{ gridTemplateColumns: `repeat(${numCols}, minmax(0, 1fr))` }}
+      style={{
+        gridTemplateColumns: `repeat(${grid[0].length}, minmax(0, 1fr))`,
+      }}
     >
       {grid.map((rows, i) =>
         rows.map((col, k) => (
           <div
-            className="w-8 h-8 bg-gray-600 active:bg-sky-800 rounded-md transition-all hover:scale-105 active:scale-100"
+            className={classNames(
+              grid[i][k] ? 'bg-sky-500' : 'bg-gray-600',
+              'w-9 h-9 active:bg-sky-800 rounded-md transition-all hover:scale-105 active:scale-100'
+            )}
+            onClick={() => {
+              const newGrid = produce(grid, (gridCopy) => {
+                gridCopy[i][k] = grid[i][k] ? 0 : 1
+              })
+              setGrid(newGrid)
+            }}
             key={`${i}-${k}`}
           />
         ))
